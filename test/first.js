de51df8ae3097d18d42c592791f91131da2ed7f7
Eddort <webisforce@gmail.com>
2018-06-22 18:01:20 +0300

    add preprocessing

--- test/first.js
+++ test/first.js
@@ -10,13 +10,11 @@ block(
         id='id-1-2-4'
     )
     gallery(
-        images=''
+        images='2'
     )
     - var images = ['/src1', '/src2', '/src3']
-    each src in images
-        image(
-            src=src
-        )
+    each srcc, i in images
+        image(src=i)
 `;
 
 const parse = require('../index')
@@ -24,20 +22,35 @@ const parse = require('../index')
 console.log(parse({
     template,
     kitTagModels: {
+        //кжадая модель вернет функцию которая выполнится
+        //в контексте шаблонизатора, таким образом подставит 
+        //необходимые
+        //node_modules/pug-runtime/index.js:function pug_attr(key, val, escaped, terse) {
+            //переписать pug_attr
+            //что бы возращал значение key:value а не key=value
         block: {
           render(params) {
             // console.log(params)
-            return `block, method ${params}`
+           
+            // return ` pug_attr("srsc", i, true, false) `
+            return `'block' + pug_attr("${params[0].name}", ${params[0].val}, true, false) `
           }
         },
         gallery: {
           render(params) {
-            return `gallery, method ${params}`
+            //   params.forEach(element => {
+            //    console.log(element)
+            // });
+            return `'gallery' + pug_attr("${params[0].name}", ${params[0].val}, true, false) `
           }
         },
         image: {
           render(params) {
-            return `image, method ${params}`
+            // params.forEach(element => {
+            //    console.log(element)
+            // });
+            // console.log(params[0],typeof params[0], params[0].name, '!!!!!!')
+            return `'image' + pug_attr("${params[0].name}", ${params[0].val}, true, false) `
           }
         }
       }
