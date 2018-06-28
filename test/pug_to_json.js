a0afcff5af2bba4f813f790054ee33de7b5b29a9
Eddort <webisforce@gmail.com>
2018-06-28 06:19:32 +0300

    need to complete the formation of strings with variables

--- test/pug_to_json.js
+++ test/pug_to_json.js
@@ -17,22 +17,26 @@ const tags = [
 ];
 
 //todo нужно парсить текст контент иначе
+
+//todo придумать как парсить кастомные массивы
+
 // | Hey, check out 
 // a(href="http://example.biz/kitteh.png") this picture
 // |  of my cat!
-const template = `
-html(lang='en')  
-	head
-		title Pug
-	body
-		h1 Pug Examples
-		div.container
-		p(aa=123) Cool Pug example!
-		- var n = 0;
-		ul
-			each n in [1,2,3,4]
-				li(ss=n) sssss #{n}
-`;
+// const template = `
+// html(lang='en')  
+// 	head
+// 		title Pug
+// 	body
+// 		h1 Pug Examples
+// 		div.container
+// 		p(aa=123) Cool Pug example! #{1+1}
+// 		- var n = 0;
+// 		ul
+// 			each n in [1,2,3,4]
+// 				li(ss=n) sssss #{n}
+// `;
+const template = `p(aa=123) Cool Pug example! #{[1,2,3]}`
 //TODO parent
 console.log(
 	parse(
